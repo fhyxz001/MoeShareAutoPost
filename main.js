d120458f0890fa7a1b4a82a317f58fb652caf56a
hexiaobin <919760638@qq.com>
2023-12-01 08:35:48 +0800

    样式优化以及自购证明信息补充

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 // ==UserScript==
 // @name         MoeShareAutoPost
 // @namespace    https://github.com/fhyxz001/MoeShareAutoPost
-// @version      0.2
+// @version      0.1
 // @description  萌享论坛资源区发贴格式化工具，用于帮助用户方便的进行资源发布
-// @author       hexbkyoma
+// @author       hexbkyoma&DIBAO
 // @match        https://moeshare.cc/post.php?fid=22
 // @match        https://moeshare.cc/post.php?fid=33
 // @match        https://moeshare.cc/post.php?fid=28
+// @match        https://www.moeshare.cc/post.php?fid=22
+// @match        https://www.moeshare.cc/post.php?fid=33
+// @match        https://www.moeshare.cc/post.php?fid=28
 // @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
 // @license MIT
+// @downloadURL https://update.greasyfork.org/scripts/481070/MoeShareAutoPost.user.js
+// @updateURL https://update.greasyfork.org/scripts/481070/MoeShareAutoPost.meta.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     // 创建一个按钮
     var newButton = document.createElement("i");
-    newButton.innerHTML = "快捷发帖";
+    newButton.innerHTML = "发帖模板";
     newButton.onclick = autoPost;
     // 找到按钮容器
     var buttonContainer = document.querySelector(".wy_top_but");
@@ -24,11 +29,11 @@
     // 添加点击按钮时触发的函数
     function autoPost() {
         //弹出一个空白的窗口，用于填写信息 [作者] 漫画书名 [卷数][出版社][提取平台][网盘名称][其他信息]
-        var postWindow = window.open("","","width=500,height=630");
+        var postWindow = window.open("","","width=580,height=680");
         //postWindow的位置在屏幕中央的偏右
         postWindow.moveTo((window.screen.width-500)/2, (window.screen.height-500)/2);
         //在窗口中添加一个标题
-        postWindow.document.title = "快捷发帖";
+        postWindow.document.title = "发帖模板";
         //标题输入框
         GenerateInput(postWindow,"作者","author","请输入");
         GenerateInput(postWindow,"漫画书名","book","请输入漫画名称");
@@ -38,31 +43,36 @@
         GenerateInput(postWindow,"网盘名称","disk","例如：BD、百度");
         GenerateInput(postWindow,"其他信息","other","备注信息，可选项");
         //在窗口中添加一个按钮
+
         var button = postWindow.document.createElement("button");
+        var br = postWindow.document.createElement("br");
         button.innerHTML = "生成标题";
         button.onclick = titleCreate;
+        button.style = "padding: 3px 8px; text-align: center; font-size: 18px; margin: 6px 110px; cursor: pointer";
         postWindow.document.body.appendChild(button);
-        postWindow.document.body.appendChild(postWindow.document.createElement("br"));
+        postWindow.document.body.appendChild(br);
 
         // 封面区输入框
-        GenerateInput(postWindow,"封面","cover","请输入封面文件图床链接");
+        GenerateInput(postWindow,"封面","cover","请输入封面文件的图床链接");
         // 简介区输入框
-        GenerateTextArea(postWindow,"简介","info","请输入简介内容","200px","80%");
-        // GenerateURL(postWindow,"moelist","moelistURL","","200px","80%");
+        GenerateTextArea(postWindow,"简介","info","请在此输入简介内容","200px","80%");
+        //GenerateURL(postWindow,"moelist","moelistURL","","200px","80%");
         // moelist区
-        // GenerateTextArea(postWindow,"moelist","moelist","请输入moelist信息","200px","80%");
+        //GenerateTextArea(postWindow,"moelist","moelist","请输入moelist信息","200px","80%");
 
         // 出售区输入框
-        GenerateInput(postWindow,"国库卷","sell","例如：0，代表0购买");
+        GenerateInput(postWindow,"售价(国库券)","sell","例如：0，代表帖子售价为0国库券");
         // MD可见区输入框
-        GenerateInput(postWindow,"MD可见","md","例如:20,代表20MD可见");
+        GenerateInput(postWindow,"MD限制","md","例如：20，代表隐藏内容需20MD以上可见");
         // 下载链接区输入框
-        GenerateTextArea(postWindow,"下载链接","download","请在此处输入下载链接及解压密码信息","50px","80%");
+        GenerateTextArea(postWindow,"受限内容","download","可在此处输入下载链接、解压密码等信息","50px","80%");
+        GenerateInput(postWindow,"自购证明","certification","请输入自购证明的图床链接");
 
         //生成简介按钮区**********
         var button1 = postWindow.document.createElement("button");
-        button1.innerHTML = "生成简介";
+        button1.innerHTML = "生成帖子";
         button1.onclick = infoCreate;
+        button1.style = "padding: 3px 8px; text-align: center; font-size: 18px; margin: 6px 110px; cursor: pointer";
         postWindow.document.body.appendChild(button1);
         postWindow.document.body.appendChild(postWindow.document.createElement("br"));
 
@@ -75,8 +85,8 @@
             finalInfo += "\n";
             finalInfo += postWindow.document.getElementById("info").value;
             finalInfo += "\n";
-            // finalInfo += postWindow.document.getElementById("moelist").value;
-            // finalInfo += "\n";
+            //finalInfo += postWindow.document.getElementById("moelist").value;
+            //finalInfo += "\n";
             if(postWindow.document.getElementById("md").value != ""){
                 finalInfo += "本下载链接需要"+postWindow.document.getElementById("md").value+"MD才能查看"+"\n";
                 if(postWindow.document.getElementById("sell").value != ""){
@@ -98,6 +108,11 @@
                 }else {
                     finalInfo += postWindow.document.getElementById("download").value;
                 }
+            }
+            if(postWindow.document.getElementById("certification").value != "") {
+                finalInfo += "自购证明：\n";
+                finalInfo += "[hide=999,credit][img]" +
+                    postWindow.document.getElementById("certification").value + "[/img][/hide]";
             }
             info.value = finalInfo;
         }
@@ -151,6 +166,7 @@
     }
     function GenerateTextArea(postWindow,labelString,textareaId,placeholderString,height,width){
         var authorLabel = postWindow.document.createElement("label");
+        if (textareaId == "download") {authorLabel.style.color = "red";}
         authorLabel.innerHTML = labelString+"：";
         authorLabel.style.display = "inline-block";
         authorLabel.style.width = "20%";
@@ -161,6 +177,7 @@
         textarea.placeholder = placeholderString;
         textarea.style.height = height;
         textarea.style.width = width;
+        textarea.style.verticalAlign = "middle";
         postWindow.document.body.appendChild(textarea);
         postWindow.document.body.appendChild(postWindow.document.createElement("br"));
     }
